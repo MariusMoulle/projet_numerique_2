555303fdece44ac9973d265c6a03070c8b90a003
apierfitte <auguste.pierfitte@gmail.com>
2020-12-08 11:38:09 +0100

    tracé des portraits de phase

--- projet.py
+++ projet.py
@@ -5,6 +5,7 @@ dx2/dt = -x2*(gamma-delta*x1)
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
 # définition des constantes
@@ -13,7 +14,8 @@ alpha, beta, gamma, delta = 2/3, 4/3, 1, 1
 def f(x1, x2):
     return np.array([x1*(alpha - beta*x2), -x2*(gamma - delta*x1)])
 
-def affiche_champ_de_vecteur(limite, couleur = "orange"):
+def affiche_champ_de_vecteur(limite = 10, couleur = "orange"):
+
     x = np.linspace(0,limite,limite + 1)
     y = np.linspace(0, limite,limite + 1)
 
@@ -23,3 +25,39 @@ def affiche_champ_de_vecteur(limite, couleur = "orange"):
 
     plt.quiver(X, Y, Z[0], Z[1], color = couleur)
     plt.show()
+
+def affiche_portrait_de_phase(limite = 10):
+
+    x = np.linspace(0,limite,limite + 1)
+    y = np.linspace(0, limite,limite + 1)
+
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+
+    U, V = Z[0], Z[1]
+    vitesse = np.sqrt(U**2 + V**2)
+
+    
+    fig = plt.figure(figsize=(7, 9))
+    gs = gridspec.GridSpec(nrows=3, ncols=2, height_ratios=[1, 1, 2])
+
+    #  changer la densité le long d'une ligne de champ
+    ax0 = fig.add_subplot(gs[0, 0])
+    ax0.streamplot(X, Y, U, V, density=[0.5, 1])
+    ax0.set_title('Variation de la densité')
+
+    # changer la couleur le long d'une ligne
+    ax1 = fig.add_subplot(gs[0, 1])
+    strm = ax1.streamplot(X, Y, U, V, color=U, linewidth=2, cmap='Purples')
+    fig.colorbar(strm.lines)
+    ax1.set_title('Variation de la couleur')
+
+    #  changer la largeur le long d'une ligne
+    ax2 = fig.add_subplot(gs[1, 0])
+    lw = 5*vitesse / vitesse.max()
+    ax2.streamplot(X, Y, U, V, density=0.6, color='orange', linewidth=lw)
+    ax2.set_title('Variation de la largeur')
+
+    plt.tight_layout()
+    plt.show()
+
